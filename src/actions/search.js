6c1b331708cdfbf6e9c91ab0c38cc457c864a219
Daniel Wolfgang Malamud-Roam <dmalamudroam@gmail.com>
2020-12-09 17:01:45 -0800

    Actions, Reducers and Store complete

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,18 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
+  var query = {
+    key: YOUTUBE_API_KEY,
+    query: q,
+  };
+
+  return (dispatch) => {
+    searchYouTube(query, (data)=>{
+      dispatch(changeVideoList(data));
+      dispatch(changeVideo(data[0]));
+    });
+  };
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,8 @@
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  type: 'CHANGE_VIDEO_LIST',
+  // pass new video object to update video player
+  videos: videos
 });
 
 export default changeVideoList;
